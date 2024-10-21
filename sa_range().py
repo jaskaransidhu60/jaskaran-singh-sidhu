abfa193c380bfa0ee777ef17f5fb132a7ce9aa40
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-10-21 16:40:00 -0700

    Update sa_range().py

--- sa_range().py
+++ sa_range().py
@@ -1,5 +1,5 @@
-def sa_range(start, end):
-    """Creates a StaticArray with all integers from start to end inclusive."""
+def sa_range(start: int, end: int) -> StaticArray:
+    """Creates a StaticArray with all integers between start and end."""
     length = abs(end - start) + 1
     result = StaticArray(length)
     step = 1 if start <= end else -1
