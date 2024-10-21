c40a1fa115cd6b90061c5989d4f9db0b86d69b1a
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-10-21 16:38:51 -0700

    Update reverse().py

--- reverse().py
+++ reverse().py
@@ -1,9 +1,7 @@
-def reverse(arr):
-    """Reverses the StaticArray in-place."""
+def reverse(arr: StaticArray) -> None:
+    """Reverses the StaticArray in place."""
     left, right = 0, arr.length() - 1
     while left < right:
-        arr.set(left, arr.get(left) ^ arr.get(right))
-        arr.set(right, arr.get(left) ^ arr.get(right))
-        arr.set(left, arr.get(left) ^ arr.get(right))
+        arr[left], arr[right] = arr[right], arr[left]
         left += 1
         right -= 1
