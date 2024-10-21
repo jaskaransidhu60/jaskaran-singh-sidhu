76e52f705f011cd4a4bdd68c1d27c26e3f13a0aa
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-10-21 16:30:48 -0700

    Create sorted_squares().py

---
+++ sorted_squares().py
@@ -0,0 +1,16 @@
+def sorted_squares(arr):
+    """Returns a StaticArray with squares of elements sorted."""
+    n = arr.length()
+    result = StaticArray(n)
+    left, right, index = 0, n - 1, n - 1
+    while left <= right:
+        left_square = arr.get(left) ** 2
+        right_square = arr.get(right) ** 2
+        if left_square > right_square:
+            result.set(index, left_square)
+            left += 1
+        else:
+            result.set(index, right_square)
+            right -= 1
+        index -= 1
+    return result
